0ec8a1e4adfbbc719610387bcb1d5a109edfe461
msaidguven <msaidguven@gmail.com>
2021-01-19 21:30:12 +0200

    msaidguven yorum

--- dangerfile.js
+++ dangerfile.js
@@ -6,6 +6,7 @@
  */
 
 'use strict';
+// deneme deneme deneme msaidguven
 
 // Hi, if this is your first time editing/reading a Dangerfile, here's a summary:
 // It's a JS runtime which helps you provide continuous feedback inside GitHub.
